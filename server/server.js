ca58074a6aa01f7ad46c72ca0d1137889109eaa2
ebaskom <ebaskom@gmail.com>
2018-01-11 13:32:40 +0700

    get todos

--- server/server.js
+++ server/server.js
@@ -23,6 +23,15 @@ app.post('/todos',(req,res)=> {
     });
 });
 
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todo) => {
+        res.send({todo});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
 });

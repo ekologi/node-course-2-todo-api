916f02641db85059fdfa5e5ac6ac392de38ffb1c
ebaskom <ebaskom@gmail.com>
2018-01-11 10:59:10 +0700

    belajar postman

--- server/server.js
+++ server/server.js
@@ -1,43 +1,47 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var { User } = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }, 
-    completedAt: {
-        type: Number,
-        default: null
-    }
+var app = express();
+
+app.use(bodyParser.json());
+//ada 2 argument, 1 adalah URL dan 2. adalah callback dgn req + res
+app.post('/todos',(req,res)=> {
+    //console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc)=>{
+        //console.log('Save sukses',doc);
+        res.send(doc);
+    }, (e)=>{
+        //console.log('Simpan gagal', e);
+        res.status(400).send(e);
+    });
 });
 
-var User = mongoose.model('User' , {
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1
-    }
+app.listen(3000, ()=>{
+    console.log('Server is running on port 3000');
 });
 
-var newUser = new User ({
-    email: '    ebaskom@gmail.com'
-});
 
-newUser.save().then((doc)=> {
-    console.log('Simpan sukses',doc)
-}, (e) => {
-    console.log('Simpan gagal ' , e);
-});
+
+
+
+
+//# LATIHAN yg SEBELUMNYA
+// var newUser = new User ({
+//     email: '    ebaskom@gmail.com'
+// });
+
+// newUser.save().then((doc)=> {
+//     console.log('Simpan sukses',doc)
+// }, (e) => {
+//     console.log('Simpan gagal ' , e);
+// });
 // var newTodo = new Todo({
 //     text: 'Cook Dinner'
 // });

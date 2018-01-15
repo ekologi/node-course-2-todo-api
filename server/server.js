72b33495d162ef6e8f4e75c829752722044a125c
ebaskom <ebaskom@gmail.com>
2018-01-15 14:38:45 +0700

    Add GET user/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var { User } = require('./models/user');
-
+var { authenticate } = require('./middleware/authenticate');
 var app = express();
 
 const port = process.env.PORT || 3000;
@@ -103,19 +103,10 @@ app.post('/users', (req, res) => {
     })
 });
 
-// //PUNYA GW
-// app.post('/users', (req, res) => {
-//     var body = _.pick(req.body, ['email', 'password']);
-//     var user = new User(body);
 
-//     user.save().then(() => {
-//         return user.generateAuthToken();
-//     }).then((token) => {
-//         res.header('x-auth', token).send(user);
-//     }).catch((e) => {
-//         res.status(400).send(e);
-//     })
-// });
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
 
 app.listen(port, ()=>{
     console.log('Server is running on port ' + port);
